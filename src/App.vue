7528633cba58031363abf95af8567ded9b19348b
denismend <denisladeira1@gmail.com>
2021-01-20 19:51:39 -0300

    disabled sample

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,37 @@
 <template>
-  <h1 v-html="title"></h1>
+  <!-- <h1 v-html="title"></h1>
   <p v-if="isShowing">v-if directive</p>
   <p v-else-if="isShowing === null">v-else-if directive</p>
   <p v-else>v-else directive</p>
   <h2>{{ count }}</h2>
   <h2 v-once>{{ count }}</h2>
-  <p v-text="text" /> 
+  <p v-text="text" />  -->
+  <h1>Heros</h1>
+  <ul>
+    <li v-for="(hero, index) in heros" :key="index">{{ index }} : {{ hero.name }}</li>
+  </ul>
+  <input type="text" :[attribute]="newHero"/>
+  <button :disabled="isDisable">Add Hero</button>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      title: '<h1>Vuejs Title</h1>',
-      isShowing: null,
-      count: 0,
-      text: "v-text is the same of interpolation"
+      // title: '<h1>Vuejs Title<  /h1>',
+      // isShowing: null,
+      // count: 0,
+      // text: "v-text is the same of interpolation",
+      attribute: "value",
+      isDisable: true ,
+      heros: [{
+        name: 'Batman',
+      }, {
+        name: 'Spider-man'
+      }, {
+        name: 'Worder Woman'
+      }],
+      newHero: 'Aqua-man'
     }
   },
   mounted() {

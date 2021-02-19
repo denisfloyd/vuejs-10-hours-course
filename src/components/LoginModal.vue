d240ebc164e30608ded6f38808fb098d74ddb39b
denismend <denisladeira1@gmail.com>
2021-02-19 09:06:46 -0300

    login form

--- src/components/LoginModal.vue
+++ src/components/LoginModal.vue
@@ -9,6 +9,37 @@
         <div class="z-30 m-auto bg-white p-2 rounded shadow w-10/12 md:w-1/3">
           <div class="p-2 border">
             <h1 class="text-2xl text-center">Login</h1>
+
+            <form class="p-2 my-2" @submit.prevent="submit">
+              <div class="my-4">
+                <label>Email or Username</label>
+                <input
+                  ref="emailRef"
+                  v-model="email"
+                  class="rounded shadow p-2 w-full"
+                  placeholder="Enter your email or username"
+                />
+              </div>
+              <div class="my-4">
+                <label>Password</label>
+                <input
+                  v-model="password"
+                  class="rounded shadow p-2 w-full"
+                  type="password"
+                  placeholder="enter your password"
+                />
+              </div>
+              <div class="my-4">
+                <button
+                  type="submit"
+                  class="w-full rounded shadow-md bg-gradient-to-r from-red-800 to-pink-800 text-white p-2"
+                >
+                  <span v-if="!isLoading">Login</span>
+                  <span v-else>⌛</span>
+                </button>
+              </div>
+            </form>
+            
           </div>
         </div>
       </div>

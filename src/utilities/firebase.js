b4faa8c98daa8aaf749892cb32a42ea9f1f59a76
denismend <denisladeira1@gmail.com>
2021-03-01 19:20:16 -0300

    add firebase

---
+++ src/utilities/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase/app';
+
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC606zwVrZoGs0ZebOI7FmAqba64CPb1Ug",
+  authDomain: "vue-full-course-e7877.firebaseapp.com",
+  projectId: "vue-full-course-e7877",
+  storageBucket: "vue-full-course-e7877.appspot.com",
+  messagingSenderId: "180208214677",
+  appId: "1:180208214677:web:4241aed4576b66e467319c"
+}
+
+firebase.inicializeApp(firebaseConfig);
+
+export default firebase;
